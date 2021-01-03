401aca52445427eff17ba6f8648262f2c2abfa5c
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-03 09:48:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ let add = (p1,p2) => p1+p2
 
 let square=nb => nb*nb
 
+<<<<<<< HEAD
 let divide = () => 2000/100;
+=======
+let divide = nb => nb/100
+>>>>>>> 578a9e8d4bfa7ca76f43ca41c9aae99da1771d3d
